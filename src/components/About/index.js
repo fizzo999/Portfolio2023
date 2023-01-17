4f147cd13d0e5867bd134800c9510ca540e46a70
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-16 23:30:44 -0800

    cube animation strated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,36 @@
+import {
+  faAngular,
+  faCss3,
+  faGit,
+  faGitAlt,
+  faGitSquare,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  setTimeout(() => {
+    setLetterClass('text-animate-hover');
+  }, 3000);
+
   const splitLetters = 'About me'.split('');
-  console.log(splitLetters, 'here is split letters');
 
   return (
     <div className="container about-page">
       <div className="text-zone">
         <h1>
-          <AnimatedLetters strArray={splitLetters} idx={15} />
+          <AnimatedLetters
+            strArray={splitLetters}
+            idx={15}
+            letterClass={letterClass}
+          />
         </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
@@ -35,6 +56,28 @@ const About = () => {
           aperiam itaque velit incidunt perferendis facilis harum accusamus
           aspernatur, cupiditate est laborum modi?
         </p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+          </div>
+        </div>
       </div>
     </div>
   );
