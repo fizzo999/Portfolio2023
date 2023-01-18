da775596cba58d6c3dd3573f29d455479a82a666
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-17 16:38:10 -0800

    adds cube animation finished and FIXED, adds pacman loader to about and home pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,8 @@ import {
   faReact,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
+import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
@@ -23,63 +24,67 @@ const About = () => {
   const splitLetters = 'About me'.split('');
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={splitLetters}
-            idx={15}
-            letterClass={letterClass}
-          />
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
-          mollitia magni nostrum, voluptatibus veniam doloribus recusandae quod
-          omnis temporibus id molestias laudantium, eos, ab odio at nulla totam
-          officiis ducimus? Labore laboriosam doloremque quas recusandae nisi
-          aperiam itaque velit incidunt perferendis facilis harum accusamus
-          aspernatur, cupiditate est laborum modi?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
-          mollitia magni nostrum, voluptatibus veniam doloribus recusandae quod
-          omnis temporibus id molestias laudantium, eos, ab odio at nulla totam
-          officiis ducimus? Labore laboriosam doloremque quas recusandae nisi
-          aperiam itaque velit incidunt perferendis facilis harum accusamus
-          aspernatur, cupiditate est laborum modi?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
-          mollitia magni nostrum, voluptatibus veniam doloribus recusandae quod
-          omnis temporibus id molestias laudantium, eos, ab odio at nulla totam
-          officiis ducimus? Labore laboriosam doloremque quas recusandae nisi
-          aperiam itaque velit incidunt perferendis facilis harum accusamus
-          aspernatur, cupiditate est laborum modi?
-        </p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#DD0031" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+    <Fragment>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={splitLetters}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
+            mollitia magni nostrum, voluptatibus veniam doloribus recusandae
+            quod omnis temporibus id molestias laudantium, eos, ab odio at nulla
+            totam officiis ducimus? Labore laboriosam doloremque quas recusandae
+            nisi aperiam itaque velit incidunt perferendis facilis harum
+            accusamus aspernatur, cupiditate est laborum modi?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
+            mollitia magni nostrum, voluptatibus veniam doloribus recusandae
+            quod omnis temporibus id molestias laudantium, eos, ab odio at nulla
+            totam officiis ducimus? Labore laboriosam doloremque quas recusandae
+            nisi aperiam itaque velit incidunt perferendis facilis harum
+            accusamus aspernatur, cupiditate est laborum modi?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio
+            mollitia magni nostrum, voluptatibus veniam doloribus recusandae
+            quod omnis temporibus id molestias laudantium, eos, ab odio at nulla
+            totam officiis ducimus? Labore laboriosam doloremque quas recusandae
+            nisi aperiam itaque velit incidunt perferendis facilis harum
+            accusamus aspernatur, cupiditate est laborum modi?
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              <span className="icon-text">CSS</span>
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGit} color="#EC4D28" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </Fragment>
   );
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import Loader from 'react-loaders';
 import { Link } from 'react-router-dom';
 // import myPicture from '../../../src/assets/images/FizzoPannosch_Thank_You.gif';
 // import LogoTitle from '../../assets/images/logo-s.png';
@@ -19,40 +20,43 @@ const Home = () => {
   }, 4000);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone animate__animated">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={greetingArray}
-            idx={13}
-          />
-          <br />
-          {/* <img src={myPicture} alt="developer" style={{}}/> */}
-          {/* <img src={LogoTitle} alt="developer" style={{}} /> */}
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Frontend Developer / JavaScript Expert / React Expert </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <Fragment>
+      <div className="container home-page">
+        <div className="text-zone animate__animated">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={greetingArray}
+              idx={13}
+            />
+            <br />
+            {/* <img src={myPicture} alt="developer" style={{}}/> */}
+            {/* <img src={LogoTitle} alt="developer" style={{}} /> */}
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Frontend Developer / JavaScript Expert / React Expert </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        {/* <Logo /> */}
+        <LogoSlobodan />
       </div>
-      {/* <Logo /> */}
-      <LogoSlobodan />
-    </div>
+      <Loader type="pacman" />
+    </Fragment>
   );
 };
 
