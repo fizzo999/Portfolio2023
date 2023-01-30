164ef9c19aae16737f173f8117cd6d4dd2229143
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-29 23:33:33 -0800

    logo change from too small/pixelated iso file to png letter F inside a frame - animated pulsating AND color matched (from straight black) - AMAZING

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,14 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoF from '../../assets/images/Letter-F-favicon.ico';
+// import LogoF from '../../assets/images/Letter-F-favicon.ico';
+import LogoF from '../../assets/images/F-LOGO-002.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faF,
+} from '@fortawesome/free-solid-svg-icons';
 import {
   faGithub,
   faLinkedin,
@@ -19,6 +25,10 @@ const Sidebar = () => {
           alt="logo"
           title="welcome to my website: Fizzo Pannosch | web developer"
         />
+        {/* <FontAwesomeIcon icon="fa-solid fa-f" color="#4d4d4e" /> */}
+        {/* <FontAwesomeIcon icon={faFeatherPointed} color="#4d4d4e" /> */}
+        {/* <FontAwesomeIcon icon={faF} color="#ffffcc" /> */}
+        {/* <FontAwesomeIcon icon={faF} /> */}
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
