9384d32dab896f8be1b1096a8065ba4a0e8f7e4a
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-02-04 00:45:53 -0800

    progress on portfolio page - mapping through 5 mp4 and display them as a card

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import './index.scss';
+
+const Card = () => {
+  return (
+    <Fragment>
+      <div className="card">
+        <h1>hello from CARD</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Card;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,53 @@
-import { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Loader from 'react-loaders';
+import { Link } from 'react-router-dom';
+
+import AnimatedLetters from '../AnimatedLetters';
+import Card from '../Card';
 import './index.scss';
 // import OlympiaCapitolLake from '../../assets/images/Olympia_Capitol_Lake_small.jpg';
+import video1 from '../../assets/images/travelDestinations.mp4';
+import video2 from '../../assets/images/slackBot.mp4';
+import video3 from '../../assets/images/music-lyfe.mp4';
+import video4 from '../../assets/images/toDoApp.mp4';
+import video5 from '../../assets/images/storeFront.mp4';
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  const videoArray = [video1, video2, video3, video4, video5];
+
+  setTimeout(() => {
+    setLetterClass('text-animate-hover');
+  }, 4000);
+
   return (
     <Fragment>
       <div className="portfolio-banner">
         FIZZO PANNOSCH <br />
         <span className="web-dev">WEB DEVELOPER</span>
       </div>
+      <div className="container portfolio-page">
+        <div className="text-zone animate__animated">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'My Portfolio'.split('')}
+              idx={13}
+            />
+            <br />
+          </h1>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <div className="card-container">
+          {videoArray.map((eachVideo) => {
+            return <Card video={eachVideo} />;
+          })}
+        </div>
+      </div>
+      <Loader type="pacman" />
     </Fragment>
   );
 };
